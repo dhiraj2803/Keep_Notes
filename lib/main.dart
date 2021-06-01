7e98630debcb9fe5dc3c045c1a93a6e21c2c90ca
dhiraj2803 <dheerajgupta2800@gmail.com>
2021-06-02 01:44:16 +0530

    code reformat

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:notes/screens/Home_Screens.dart';
 import 'package:notes/models/NotesProvider.dart';
-
+import 'package:notes/screens/Home_Screens.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context)=>NotesProviders(),
+      create: (context) => NotesProviders(),
       child: MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
-            primarySwatch: Colors.blue ,
+            primarySwatch: Colors.blue,
             visualDensity: VisualDensity.adaptivePlatformDensity,
           ),
-          home: Home_Screen()
-      ),
+          home: Home_Screen()),
     );
   }
 }
